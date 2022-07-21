d6c68c5146c039caee2bedc8e0a55dcf8ad6153e
abdirizak osman <abdirizakosman123@gmail.com>
2022-07-21 21:11:40 +0100

    updated IMS class for order

--- src/main/java/com/qa/ims/IMS.java
+++ src/main/java/com/qa/ims/IMS.java
@@ -6,8 +6,10 @@ import org.apache.logging.log4j.Logger;
 import com.qa.ims.controller.Action;
 import com.qa.ims.controller.CrudController;
 import com.qa.ims.controller.CustomerController;
+import com.qa.ims.controller.OrderController;
 import com.qa.ims.controller.itemController;
 import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.OrderDAO;
 import com.qa.ims.persistence.dao.itemDAO;
 import com.qa.ims.persistence.domain.Domain;
 import com.qa.ims.utils.DBUtils;
@@ -19,14 +21,19 @@ public class IMS {
 
 	private final CustomerController customers;
 	private final itemController items;
+	private final OrderController orders;
 	private final Utils utils;
 
 	public IMS() {
 		this.utils = new Utils();
 		final CustomerDAO custDAO = new CustomerDAO();
 		final itemDAO ItemDAO = new itemDAO();
+		final OrderDAO orderDAO = new OrderDAO(custDAO, ItemDAO);
 		this.customers = new CustomerController(custDAO, utils);
 		this.items = new itemController(ItemDAO, utils);
+		this.orders = new OrderController(orderDAO, utils);
+		
+		
 	}
 
 	public void imsSystem() {
@@ -58,6 +65,7 @@ public class IMS {
 				active = this.items;
 				break;
 			case ORDER:
+				active = this.orders;
 				break;
 			case STOP:
 				return;
